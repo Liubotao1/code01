ba71a3461ec35be03e96042113142d0baa85a795
Liubotao1 <18392616412@163.com>
2022-06-02 14:36:55 +0800

    类型转换

---
+++ src/AssignFirst.java
@@ -0,0 +1,9 @@
+public class AssignFirst {
+    public static void main(String[] args){
+        int a = 0;
+        int b = a+1;
+        System.out.println(b);
+
+    }
+
+}
---
+++ src/CalcAndAssign.java
@@ -0,0 +1,11 @@
+public class CalcAndAssign {
+    public static void main(String[] args){
+        int a = 16;
+        a >>= 2; //右移两位
+        System.out.println(a);
+        //  此时a为4；
+        a *=9;
+        System.out.println(a);
+
+    }
+}
